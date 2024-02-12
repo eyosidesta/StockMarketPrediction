621fd0a0a038ffa6dfa0da0b52f7f8c1b8e9a14d
eyosidesta <mamesmsdesta@gmail.com>
2024-02-12 05:15:27 -0500

    import libraries

---
+++ StockMarketPrediction/AppleIncStockPrediction.py
@@ -0,0 +1,6 @@
+import pandas as pd
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import MinMaxScaler
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import Dense
+
