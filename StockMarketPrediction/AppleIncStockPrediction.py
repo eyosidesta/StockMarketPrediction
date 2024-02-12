b645860d2f24cd7086aa55b87ee3e073290492a0
eyosidesta <mamesmsdesta@gmail.com>
2024-02-12 10:34:39 -0500

    Apple Stock Market Prediction Model added

--- StockMarketPrediction/AppleIncStockPrediction.py
+++ StockMarketPrediction/AppleIncStockPrediction.py
@@ -1,38 +1,90 @@
 import pandas as pd
-import os
+import numpy as np
 from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import MinMaxScaler
+from sklearn.metrics import mean_squared_error
 from tensorflow.keras.models import Sequential
 from tensorflow.keras.layers import Dense
+import matplotlib.pyplot as plt
 
 
-apple_inc_file = '/Users/eyosiasdesta/AIProjects/StockMarketPrediction/StockMarketPrediction/AAPL.csv'
-df = pd.read_csv(apple_inc_file, parse_dates=['Date'], index_col='Date')
+class AppleStockPrediction:
+    def __init__(self, file):
+        self.file = file
+        self.dataFrame = None
 
-features = ["Open", "High", "Low", "Close", "Adj Close", "Volume"]
+    def importData(self):
+        self.dataFrame = pd.read_csv(self.file, parse_dates=['Date'], index_col='Date')
+    
+    def manipulateData(self):
+        headers = ['Open', 'High', 'Low', 'Close', 'Adj Close', 'Volume']
+        data = self.dataFrame[headers]
+        return data
+    
+    # use data from February 2019, to Decemeber 30, 2022 so that we can train the model with this dataset
+    # and test our model with the test dataset
+    def defineTrainData(self):
+        data = self.manipulateData()
+        define_x_train_data = data.loc['2019-02-12':'2022-12-30']
+        return define_x_train_data
+    
+    # use test data from January 3, 2023 to February 9, 2024 so that we can test our model weather our model is 
+    # performing well or not
+    def defineTestData(self):
+        data = self.manipulateData()
+        define_x_test_data = data.loc['2023-01-03':'2024-02-09']
+        return define_x_test_data
+    
+    def normalizeTrainData(self):
+        normalize = MinMaxScaler()
+        train_data = self.defineTrainData()
+        normalizeTrainData = normalize.fit_transform(train_data)
+        return normalizeTrainData
+    
+    def normalizeTestData(self):
+        normalizeScaler = MinMaxScaler()
+        test_data = self.defineTestData()
+        normalizeTestData = normalizeScaler.fit_transform(test_data)
+        return normalizeTestData, normalizeScaler
+    
+    def inputOutputTrainData(self):
+        normalizedTrainData = self.normalizeTrainData()
+        X_train = normalizedTrainData[:, :-1]
+        y_train = normalizedTrainData[:, -1]
+        return X_train, y_train
+    
+    def inputOutputTestData(self):
+        normalizedTestData, normalizeScaler = self.normalizeTestData()
+        X_test = normalizedTestData[:, :-1]
+        y_test = normalizedTestData[:, -1]
+        return X_test, y_test, normalizeScaler
+    
+    def BuildAndTrainModel(self):
+        X_train, y_train = self.inputOutputTrainData()
+        X_test, y_test, normalizeScaler = self.inputOutputTestData()
+        X_test_data = self.defineTestData()
+        model = Sequential()
+        model.add(Dense(64, input_dim=X_train.shape[1], activation='relu'))
+        model.add(Dense(1, activation='linear'))
 
-data = df[features]
+        model.compile(optimizer='adam', loss='mean_squared_error')
+        model.fit(X_train, y_train, epochs=50, batch_size=32, validation_data=(X_test, y_test))
+        
+        predictions_scaled = model.predict(X_test)
 
-scalar = MinMaxScaler()
-sclaed_data = scalar.fit_transform(data)
+        meanSquaredError = mean_squared_error(y_test, predictions_scaled)
+        print(f"Mean Squared Error is {meanSquaredError}")
+        predictions_original_scale = normalizeScaler.inverse_transform(np.concatenate([X_test, predictions_scaled], axis=1))[:, -1]
 
-X = sclaed_data[:, :-1]
-y = sclaed_data[:, -1]
-
-
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.02, random_state=42)
-
-model = Sequential()
-model.add(Dense(64, input_dim=X_train.shape[1], activation='relu'))
-model.add(Dense(1, activation='linear'))
-
-model.compile(optimizer='adam', loss='mean_squared_error')
-model.fit(X_train, y_train, epochs=50, batch_size=32, validation_data=(X_test, y_test))
-
-mse = model.evaluate(X_test, y_test)
-print(f'Mean Squared Error on Test Set: {mse}')
+        # Visualize predictions
+        plt.figure("Apple Inc Stock Market Prediction")
+        plt.plot(X_test_data.index, predictions_original_scale, label='Predicted Volume')
+        plt.plot(X_test_data.index, X_test_data['Volume'], label='Actual Volume')
+        plt.legend()
+        plt.show()
 
 
-predictions = model.predict(X_test)
-
-print(df.head())
+Apple_Inc_path = '/Users/eyosiasdesta/AIProjects/StockMarketPrediction/StockMarketPrediction/AAPL.csv'
+apple_stock = AppleStockPrediction(Apple_Inc_path)
+apple_stock.importData()
+apple_stock.BuildAndTrainModel()
